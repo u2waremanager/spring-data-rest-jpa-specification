68da012770c334eb7d616b37a34970732e7a527d
u2waremanager@gmail.com <u2waremanager@gmail.com>
2020-02-11 15:29:35 +0900

    -

--- src/test/java/io/github/u2ware/test/example4/Bar.java
+++ src/test/java/io/github/u2ware/test/example4/Bar.java
@@ -3,6 +3,7 @@ package io.github.u2ware.test.example4;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.UUID;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -12,11 +13,22 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.persistence.JoinColumn;
+import javax.persistence.ForeignKey;
+import javax.persistence.ConstraintMode;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinTable;
+
 
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.webmvc.support.UriLinkDeserializer;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import lombok.Data;
 import lombok.Getter;
@@ -46,145 +58,145 @@ public @Data class Bar {
 	///////////////////////////////////////////////////////////////////
 	// Transient ManyToOne...
 	//////////////////////////////////////////////////////////////////
-//	@Transient
-//	@JsonProperty(access=Access.WRITE_ONLY) 
-//	private Foo foo01;
-//
-//	@Transient
-//	@JsonProperty(access=Access.WRITE_ONLY) 
-//	private FooView foo02;
-//	
-//	@Transient
-//	@JsonDeserialize(using=UriLinkDeserializer.class)
-//	@JsonProperty(access=Access.WRITE_ONLY) 
-//	private Foo foo03;
-//
-//	@Transient
-//	@JsonDeserialize(using=UriLinkDeserializer.class)
-//	@JsonProperty(access=Access.WRITE_ONLY) 
-//	private FooView foo04;
+	@Transient
+	@JsonProperty(access=Access.WRITE_ONLY) 
+	private Foo foo01;
+
+	@Transient
+	@JsonProperty(access=Access.WRITE_ONLY) 
+	private FooView foo02;
+	
+	@Transient
+	@JsonDeserialize(using=UriLinkDeserializer.class)
+	@JsonProperty(access=Access.WRITE_ONLY) 
+	private Foo foo03;
+
+	@Transient
+	@JsonDeserialize(using=UriLinkDeserializer.class)
+	@JsonProperty(access=Access.WRITE_ONLY) 
+	private FooView foo04;
 	
 	
 	///////////////////////////////////////////////////////////////////
 	// Transient OneToMany...
 	//////////////////////////////////////////////////////////////////
-//	@Transient
-//	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
-//	@JsonProperty(access=Access.WRITE_ONLY) 
-//	private List<Foo> foo05;
-//
-//	@Transient
-//	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
-//	@JsonProperty(access=Access.WRITE_ONLY) 
-//	private List<FooView> foo06;
+	@Transient
+	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
+	@JsonProperty(access=Access.WRITE_ONLY) 
+	private List<Foo> foo05;
+
+	@Transient
+	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
+	@JsonProperty(access=Access.WRITE_ONLY) 
+	private List<FooView> foo06;
 
 	
 	///////////////////////////////////////////////////////////////////
 	// @ManyToOne physical foreign key
 	//////////////////////////////////////////////////////////////////
-//	@RestResource(exported=true)/*rendering link only */
-//	@ManyToOne
-//	@JoinColumn(name=/*Bar*/"foo11" , referencedColumnName=/* Foo's primary*/"id")
-//	@JsonDeserialize(using=UriLinkDeserializer.class)
-//	private Foo foo11;
-//	
-//	@RestResource(exported=false) /*rendering data only */
-//	@ManyToOne 
-//	@JoinColumn(name=/*Bar*/"foo12" , referencedColumnName=/* Foo's primary*/"id")
-//	@JsonDeserialize(using=UriLinkDeserializer.class)
-//	private Foo foo12;
-//
-//	@RestResource(exported=true) /*Don't care*/
-//	@ManyToOne 
-//	@JoinColumn(name=/*Bar*/"foo13" , referencedColumnName=/* Foo's primary*/"id")
-//	@JsonDeserialize(using=UriLinkDeserializer.class)
-//	private FooView foo13;
-//	
-//	@RestResource(exported=false) /*Don't care*/
-//	@ManyToOne 
-//	@JoinColumn(name=/*Bar*/"foo14" , referencedColumnName=/* Foo's primary*/"id")
-//	@JsonDeserialize(using=UriLinkDeserializer.class)
-//	private FooView foo14;
+	@RestResource(exported=true)/*rendering link only */
+	@ManyToOne
+	@JoinColumn(name=/*Bar*/"foo11" , referencedColumnName=/* Foo's primary*/"id")
+	@JsonDeserialize(using=UriLinkDeserializer.class)
+	private Foo foo11;
+	
+	@RestResource(exported=false) /*rendering data only */
+	@ManyToOne 
+	@JoinColumn(name=/*Bar*/"foo12" , referencedColumnName=/* Foo's primary*/"id")
+	@JsonDeserialize(using=UriLinkDeserializer.class)
+	private Foo foo12;
+
+	@RestResource(exported=true) /*Don't care*/
+	@ManyToOne 
+	@JoinColumn(name=/*Bar*/"foo13" , referencedColumnName=/* Foo's primary*/"id")
+	@JsonDeserialize(using=UriLinkDeserializer.class)
+	private FooView foo13;
+	
+	@RestResource(exported=false) /*Don't care*/
+	@ManyToOne 
+	@JoinColumn(name=/*Bar*/"foo14" , referencedColumnName=/* Foo's primary*/"id")
+	@JsonDeserialize(using=UriLinkDeserializer.class)
+	private FooView foo14;
 	
 	///////////////////////////////////////////////////////////////////
 	// @ManyToOne logical foreign column 
 	//////////////////////////////////////////////////////////////////
-//	@ManyToOne
-//	@RestResource(exported=false)
-//	@JoinColumn(name="foo21", foreignKey=/* Not exists Foo's column*/@ForeignKey(value=ConstraintMode.NO_CONSTRAINT))
-//	@JsonDeserialize(using=UriLinkDeserializer.class)
-//	private Foo foo21;
-//
-//	
-//	@ManyToOne
-//	@JoinColumn(name="foo22", foreignKey=/* Not exists Foo's column*/@ForeignKey(value=ConstraintMode.NO_CONSTRAINT))
-//	@JsonDeserialize(using=UriLinkDeserializer.class)
-//	private FooView foo22;
+	@ManyToOne
+	@RestResource(exported=false)
+	@JoinColumn(name="foo21", foreignKey=/* Not exists Foo's column*/@ForeignKey(value=ConstraintMode.NO_CONSTRAINT))
+	@JsonDeserialize(using=UriLinkDeserializer.class)
+	private Foo foo21;
+
+	
+	@ManyToOne
+	@JoinColumn(name="foo22", foreignKey=/* Not exists Foo's column*/@ForeignKey(value=ConstraintMode.NO_CONSTRAINT))
+	@JsonDeserialize(using=UriLinkDeserializer.class)
+	private FooView foo22;
 	
 	
 	///////////////////////////////////////////////////////////////////
 	// @OneToMany physical foreign key
 	//////////////////////////////////////////////////////////////////
-//	@RestResource(exported=false) 
-//	@OneToMany(fetch=FetchType.EAGER)
-//	@JoinColumn(name="foo31", referencedColumnName="id")
-//	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
-//	private Set<Foo> foo31;	
-//
-//	
-//	@OneToMany(fetch=FetchType.EAGER)
-//	@JoinColumn(name="foo32", referencedColumnName="id")
-//	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
-//	private Set<FooView> foo32;	
+	@RestResource(exported=false) 
+	@OneToMany(fetch=FetchType.EAGER)
+	@JoinColumn(name="foo31", referencedColumnName="id")
+	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
+	private Set<Foo> foo31;	
+
+	
+	@OneToMany(fetch=FetchType.EAGER)
+	@JoinColumn(name="foo32", referencedColumnName="id")
+	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
+	private Set<FooView> foo32;	
 
 	
 	///////////////////////////////////////////////////////////////////
 	// @OneToMany logical foreign column
 	//////////////////////////////////////////////////////////////////
-//	@RestResource(exported=false) 
-//	@OneToMany(fetch=FetchType.EAGER)
-//	@JoinColumn(name="foo41", foreignKey=@ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
-//	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
-//	private Set<Foo> foo41;	
-//
-//	
-//	@OneToMany(fetch=FetchType.EAGER)
-//	@JoinColumn(name="foo42", foreignKey=@ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
-//	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
-//	private Set<FooView> foo42;	
+	@RestResource(exported=false) 
+	@OneToMany(fetch=FetchType.EAGER)
+	@JoinColumn(name="foo41", foreignKey=@ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
+	private Set<Foo> foo41;	
+
+	
+	@OneToMany(fetch=FetchType.EAGER)
+	@JoinColumn(name="foo42", foreignKey=@ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
+	private Set<FooView> foo42;	
 	
 	
 	///////////////////////////////////////////////////////////////////
 	// @ManyToMany physical foreign table
 	//////////////////////////////////////////////////////////////////
-//	@RestResource(exported=false) 
-//	@ManyToMany(fetch=FetchType.EAGER)
-//	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
-//	private Set<Foo> foo51;
-//	
-//	
-//	@ManyToMany(fetch=FetchType.EAGER)
-//	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
-//	private Set<FooView> foo52;
+	@RestResource(exported=false) 
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
+	private Set<Foo> foo51;
+	
+	
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
+	private Set<FooView> foo52;
 	
 	
 	///////////////////////////////////////////////////////////////////
 	// @ManyToMany physical foreign table + logical foreign column
 	//////////////////////////////////////////////////////////////////
-//	@RestResource(exported=false) 
-//	@ManyToMany(fetch=FetchType.EAGER)
-//	@JoinTable(name="bar_foo61", 
-//		joinColumns={@JoinColumn(name="bar_id", referencedColumnName="id")},
-//		foreignKey=@ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
-//	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
-//	private Set<Foo> foo61;
-//
-//	@ManyToMany(fetch=FetchType.EAGER)
-//	@JoinTable(name="bar_foo62", 
-//		joinColumns={@JoinColumn(name="bar_id", referencedColumnName="id")},
-//		foreignKey=@ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
-//	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
-//	private Set<FooView> foo62;
+	@RestResource(exported=false) 
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JoinTable(name="bar_foo61", 
+		joinColumns={@JoinColumn(name="bar_id", referencedColumnName="id")},
+		foreignKey=@ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
+	private Set<Foo> foo61;
+
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JoinTable(name="bar_foo62", 
+		joinColumns={@JoinColumn(name="bar_id", referencedColumnName="id")},
+		foreignKey=@ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+	@JsonDeserialize(contentUsing=UriLinkDeserializer.class)
+	private Set<FooView> foo62;
 	
 
 	///////////////////////////////////////////////////////////////////
